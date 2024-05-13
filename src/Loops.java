3c460c5eb4dcac55049130b12386ad835809bea0
lautaropelozo <lautaropelozo@gmail.com>
2024-05-13 13:28:23 -0300

    curso alura

---
+++ src/Loops.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double nota = 0;
+        double mediaEvaluciones = 0;
+
+        for (int i = 0; i < 3 ; i++) {
+            System.out.println("Escribe la nota que le darias a la pelicula matrix");
+            nota = teclado.nextDouble();
+            mediaEvaluciones = mediaEvaluciones + nota;
+
+        }
+        System.out.println("La media de evaluaciones para matrix es :" + mediaEvaluciones / 3);
+    }
+}
